2b40931c11ce96ae45368818ac9020a4b46994ce
Aaron Hardy <aahardy@adobe.com>
2015-10-06 16:54:04 -0600

    Moved lint task to testrunner repo.

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,10 @@
 'use strict';
 
 var gulp = require('gulp');
-var eslint = require('gulp-eslint');
 require('turbine-gulp-testrunner')(gulp);
 
 gulp.task('watch', function() {
   //gulp.watch(['./src/{,**/!(__tests__)}/*.js']);
-});
-
-gulp.task('lint', function() {
-  return gulp.src('./src/**/*.js')
-    .pipe(eslint())
-    // eslint.format() outputs the lint results to the console.
-    // Alternatively use eslint.formatEach() (see Docs).
-    .pipe(eslint.format('stylish'))
-    // To have the process exit with an error code (1) on
-    // lint error, return the stream and pipe to failOnError last.
-    .pipe(eslint.failAfterError());
 });
 
 gulp.task('default', ['watch']);
