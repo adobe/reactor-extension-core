aa450215179fc8a17058ca2f9f326a8b54b18b7e
Aaron Hardy <aahardy@adobe.com>
2015-10-07 16:47:09 -0600

    Added turbine builder.

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,4 @@
 
 var gulp = require('gulp');
 require('turbine-gulp-testrunner')(gulp);
-
-gulp.task('watch', function() {
-  //gulp.watch(['./src/{,**/!(__tests__)}/*.js']);
-});
-
-gulp.task('default', ['watch']);
+require('turbine-gulp-builder')(gulp);
