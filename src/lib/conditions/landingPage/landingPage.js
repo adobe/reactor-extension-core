041313feb18bcd910d33056d293d5f92f13af54a
Aaron Hardy <aahardy@adobe.com>
2015-11-03 20:26:47 -0700

    Modified landing page and traffic source conditions since they only need to match against a single landing page/traffic source.

--- src/lib/conditions/landingPage/landingPage.js
+++ src/lib/conditions/landingPage/landingPage.js
@@ -4,17 +4,13 @@ var textMatch = require('textMatch');
 var visitorTracking = require('resourceProvider').get('dtm', 'visitorTracking');
 
 /**
- * Landing page condition. Determines if the actual landing page matches at least one acceptable
- * landing page.
+ * Landing page condition. Determines if the actual landing page matches an acceptable landing page.
  * @param {Object} config Condition config.
- * @param {RegEx[]} config.pages An array of acceptable landing pages.
+ * @param {RegEx[]} config.page An acceptable landing page.
  * @returns {boolean}
  */
 module.exports = function(config) {
   var landingPage = visitorTracking.getLandingPage();
-
-  return config.pages.some(function(landingPageCriterion) {
-    return textMatch(landingPage, landingPageCriterion);
-  });
+  return textMatch(landingPage, config.page);
 };
 
