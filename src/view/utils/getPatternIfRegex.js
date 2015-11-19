a137b877b50c57cd11d684339c0b210e5c7b7e6f
Aaron Hardy <aahardy@adobe.com>
2015-11-18 17:02:51 -0700

    Fixed default reg exp pattern display issue.

--- src/view/utils/getPatternIfRegex.js
+++ src/view/utils/getPatternIfRegex.js
@@ -9,7 +9,18 @@
 module.exports = function(value) {
   if (value instanceof RegExp) {
     var match = value.toString().match(new RegExp('^/(.*?)/[gimy]*$'));
-    return match ? match[1] : '';
+    var pattern = match ? match[1] : '';
+
+    // The ECMA standard specifies that regular expressions may not be empty and states that the
+    // pattern in these cases should be (?:). In other words, new RegExp().toString() === '/(?:)/'
+    // Typically we don't want to be dealing with what essentially amounts to a placeholder
+    // pattern so we will return an empty string instead.
+    // Bottom of the section: http://ecma262-5.com/ELS5_HTML.htm#Section_7.8.5
+    if (pattern === '(?:)') {
+      pattern = '';
+    }
+
+    return pattern;
   } else {
     return value;
   }
