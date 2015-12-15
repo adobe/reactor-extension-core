ccdb6d0fdeb1638b420355b1962a1c3d19a005d8
Aaron Hardy <aahardy@adobe.com>
2015-12-15 11:20:11 -0700

    Added more click view tests.

---
+++ src/view/events/__tests__/click.test.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Coral from 'coralui-support-react';
+import TestUtils from 'react-addons-test-utils';
+import { mapStateToProps } from '../click';
+import { fromJS } from 'immutable';
+import { actionCreators } from '../../actions/delayLinkActivationActions';
+import MockComponent from '../../components/__tests__/helpers/mockComponent';
+
+describe('click view', () => {
+  let Click;
+
+  let render = props => {
+    return TestUtils.renderIntoDocument(
+      <Click foo='bar' {...props} />
+    );
+  };
+
+  let getParts = component => {
+    return {
+      delayLinkActivationCheckbox:
+        TestUtils.findRenderedComponentWithType(component, Coral.Checkbox)
+    };
+  };
+
+  beforeAll(() => {
+    let inject = require('inject?' +
+      '../components/advancedEventOptions&' +
+      '../components/elementFilter' +
+      '!../click');
+    Click = inject({
+      '../components/advancedEventOptions': MockComponent,
+      '../components/elementFilter': MockComponent
+    }).Click;
+  });
+
+
+  it('maps state to props', () => {
+    let props = mapStateToProps(fromJS({
+      delayLinkActivation: true
+    }));
+
+    expect(props).toEqual({
+      delayLinkActivation: true
+    });
+  });
+
+  describe('delay link activation checkbox', () => {
+    it('is checked with delayLinkActivation=true', () => {
+      let { delayLinkActivationCheckbox } = getParts(render({
+        delayLinkActivation: true
+      }));
+
+      expect(delayLinkActivationCheckbox.props.checked).toBe(true);
+    });
+
+    it('dispatches an action when clicked', () => {
+      let dispatch = jasmine.createSpy();
+      let { delayLinkActivationCheckbox } = getParts(render({
+        dispatch
+      }));
+
+      TestUtils.Simulate.change(ReactDOM.findDOMNode(delayLinkActivationCheckbox), {
+        target: {
+          checked: true
+        }
+      });
+
+      expect(dispatch).toHaveBeenCalledWith(actionCreators.setDelayLinkActivation(true));
+    });
+  })
+});
