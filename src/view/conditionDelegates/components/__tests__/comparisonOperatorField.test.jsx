aa2308dc4b08b6c5e590d091f869b3f2b1d87c69
Aaron Hardy <aahardy@adobe.com>
2016-01-15 13:06:32 -0700

    Added tests for dataElementNameField.

--- src/view/conditionDelegates/components/__tests__/comparisonOperatorField.test.jsx
+++ src/view/conditionDelegates/components/__tests__/comparisonOperatorField.test.jsx
@@ -3,6 +3,10 @@ import Coral from 'coralui-support-react';
 import TestUtils from 'react-addons-test-utils';
 import ComparisonOperatorField from '../comparisonOperatorField';
 
+const render = props => {
+  return TestUtils.renderIntoDocument(<ComparisonOperatorField {...props}/>);
+};
+
 const getParts = instance => {
   return {
     select: TestUtils.findRenderedComponentWithType(instance, Coral.Select)
@@ -11,15 +15,17 @@ const getParts = instance => {
 
 describe('comparison operator field', () => {
   it('sets selected value on select', () => {
-    const instance = TestUtils.renderIntoDocument(<ComparisonOperatorField value="<"/>);
-    const { select } = getParts(instance);
+    const { select } = getParts(render({
+      value: '<'
+    }));
     expect(select.props.value).toBe('<');
   });
   
   it('calls onChange with value from event', () => {
     const onChange = jasmine.createSpy();
-    const instance = TestUtils.renderIntoDocument(<ComparisonOperatorField onChange={onChange}/>);
-    const { select } = getParts(instance);
+    const { select } = getParts(render({
+      onChange
+    }));
 
     select.props.onChange({
       target: {
