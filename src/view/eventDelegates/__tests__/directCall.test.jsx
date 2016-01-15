f8a97fbbd91fabf6f68101464c7df8c23e4c2695
Aaron Hardy <aahardy@adobe.com>
2016-01-15 11:29:14 -0700

    Added tests for direct call event.

---
+++ src/view/eventDelegates/__tests__/directCall.test.jsx
@@ -0,0 +1,47 @@
+import TestUtils from 'react-addons-test-utils';
+import Coral from '../../reduxFormCoralUI';
+import setupComponent from '../../__tests__/helpers/setupComponent';
+import DirectCall from '../directCall';
+import ValidationWrapper from '../../components/validationWrapper';
+
+const {instance, extensionBridge} = setupComponent(DirectCall);
+const getParts = () => {
+  return {
+    nameField: TestUtils.findRenderedComponentWithType(instance, Coral.Textfield),
+    nameValidationWrapper: TestUtils.findRenderedComponentWithType(instance, ValidationWrapper)
+  };
+};
+
+describe('direct call view', () => {
+  it('sets form values from config', () => {
+    extensionBridge.init({
+      config: {
+        name: 'foo'
+      }
+    });
+
+    const { nameField } = getParts();
+
+    expect(nameField.props.value).toBe('foo');
+  });
+
+  it('sets config from form values', () => {
+    extensionBridge.init();
+
+    const { nameField } = getParts();
+    nameField.props.onChange('foo');
+
+    expect(extensionBridge.getConfig()).toEqual({
+      name: 'foo'
+    });
+  });
+
+  it('sets errors if required values are not provided', () => {
+    extensionBridge.init();
+    expect(extensionBridge.validate()).toBe(false);
+
+    const { nameValidationWrapper } = getParts();
+
+    expect(nameValidationWrapper.props.error).toEqual(jasmine.any(String));
+  });
+});
