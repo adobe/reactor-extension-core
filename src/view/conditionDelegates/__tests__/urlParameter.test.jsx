7e52b8a57597d6a25e8ff874009c0e421daf38a3
Aaron Hardy <aahardy@adobe.com>
2016-01-15 11:23:25 -0700

    Added tests for url parameter condition.

---
+++ src/view/conditionDelegates/__tests__/urlParameter.test.jsx
@@ -0,0 +1,67 @@
+import TestUtils from 'react-addons-test-utils';
+import Coral from '../../reduxFormCoralUI';
+import setupComponent from '../../__tests__/helpers/setupComponent';
+import URLParameter from '../urlParameter';
+import ValidationWrapper from '../../components/validationWrapper';
+import RegexToggle from '../../components/regexToggle';
+
+const {instance, extensionBridge} = setupComponent(URLParameter);
+const getParts = () => {
+  const validationWrappers = TestUtils.scryRenderedComponentsWithType(instance, ValidationWrapper);
+  const textfields = TestUtils.scryRenderedComponentsWithType(instance, Coral.Textfield);
+  return {
+    nameField: textfields[0],
+    nameValidationWrapper: validationWrappers[0],
+    valueField: textfields[1],
+    valueValidationWrapper: validationWrappers[1],
+    regexToggle: TestUtils.findRenderedComponentWithType(instance, RegexToggle)
+  };
+};
+
+describe('url parameter view', () => {
+  it('sets form values from config', () => {
+    extensionBridge.init({
+      config: {
+        name: 'foo',
+        value: 'bar',
+        valueIsRegex: true
+      }
+    });
+
+    const { nameField, valueField, regexToggle } = getParts();
+
+    expect(nameField.props.value).toBe('foo');
+    expect(valueField.props.value).toBe('bar');
+    expect(regexToggle.props.value).toBe('bar');
+    expect(regexToggle.props.valueIsRegex).toBe(true);
+  });
+
+  it('sets config from form values', () => {
+    extensionBridge.init();
+
+    const { nameField, valueField, regexToggle } = getParts();
+
+    nameField.props.onChange('foo');
+    valueField.props.onChange('bar');
+    regexToggle.props.setValueIsRegex(true);
+
+    expect(extensionBridge.getConfig()).toEqual({
+      name: 'foo',
+      value: 'bar',
+      valueIsRegex: true
+    });
+  });
+
+  it('sets errors if required values are not provided', () => {
+    extensionBridge.init();
+    expect(extensionBridge.validate()).toBe(false);
+
+    const {
+      nameValidationWrapper,
+      valueValidationWrapper
+    } = getParts();
+
+    expect(nameValidationWrapper.props.error).toEqual(jasmine.any(String));
+    expect(valueValidationWrapper.props.error).toEqual(jasmine.any(String));
+  });
+});
