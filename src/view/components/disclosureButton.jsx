5a3b5e3c79744eb458f8d77cd358939af0967799
Serban Stancu <stancu@adobe.com>
2016-02-19 17:04:11 -0700

    Move arrow to the left side.

--- src/view/components/disclosureButton.jsx
+++ src/view/components/disclosureButton.jsx
@@ -7,8 +7,8 @@ export default class DisclosureButton extends React.Component {
 
     return (
       <button ref="button" className="u-buttonReset" onClick={this.props.onClick}>
+        <Coral.Icon ref="icon" className="DisclosureButton-icon u-gapRight" icon={iconClass}/>
         {this.props.label}
-        <Coral.Icon ref="icon" className="DisclosureButton-icon" icon={iconClass}/>
       </button>
     );
   }
