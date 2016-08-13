10c31d1a8b1653127b0032d24a4084aa94d5f570
Aaron Hardy <aahardy@adobe.com>
2016-08-12 20:19:05 -0600

    Degulpifying.

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,18 @@
-'use strict';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-var gulp = require('gulp');
-var webpack = require('webpack-stream');
-var sourcemaps = require('gulp-sourcemaps');
-var path = require('path');
-var plumber = require('gulp-plumber');
-var notify = require('gulp-notify');
-var argv = require('yargs')
-  .default('watch', true)
-  .alias('source-maps', 'with-source-maps')
-  .argv;
-
-// Shows an growl notification saying that building failed and then logs the error to the console.
-var errorAlert = function(error) {
-  notify.onError({
-    title: 'Build Error',
-    message: 'Check your terminal',
-    sound: 'Sosumi'
-  })(error);
-
-  console.log(error.toString());
-
-  this.emit('end');
-};
-
-var webpackConfig = {
+module.exports = {
+  entry: './src/view/index.jsx',
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/view/index.html'
+    })
+  ],
   output: {
+    path: 'dist/',
     filename: 'view.js'
   },
-  devtool: argv.sourceMaps ? '#inline-source-map' : false,
   module: {
     loaders: [
       {
@@ -68,30 +52,3 @@ var webpackConfig = {
     ]
   }
 };
-
-gulp.task('buildJS', function() {
-  var stream = gulp.src('src/view/index.jsx')
-    // Allows building to fail without breaking file watchers, etc.
-    .pipe(plumber({ errorHandler: errorAlert }))
-    .pipe(sourcemaps.init())
-    .pipe(webpack(webpackConfig));
-
-  if (argv.sourceMaps) {
-    stream = stream.pipe(sourcemaps.write('.'));
-  }
-
-  return stream.pipe(gulp.dest('dist'));
-});
-
-gulp.task('copyHTML', function() {
-  return gulp
-    .src('src/view/index.html')
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('watch', function() {
-  gulp.watch('src/view/**/*', ['buildJS']);
-  gulp.watch('src/view/**/*.html', ['copyHTML']);
-});
-
-gulp.task('buildView', ['buildJS', 'copyHTML']);
