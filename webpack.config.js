21f7265957e6b4551efb2cc96763c7cddd2dddfc
Aaron Hardy <aahardy@adobe.com>
2016-10-20 16:35:54 -0600

    Setting production environment variable during production build.

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,11 @@ plugins.push(
 
 if (argv.production) {
   plugins.push(
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
