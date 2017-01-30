afecfe9f72c0e2b8dc14a5bf43f3cb80df9f2b85
Aaron Hardy <aahardy@adobe.com>
2017-01-30 14:12:30 -0700

    Added copyright banner.

--- src/view/events/components/elementPropertiesEditor.jsx
+++ src/view/events/components/elementPropertiesEditor.jsx
@@ -1,3 +1,21 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2016 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property
+* laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
 import React from 'react';
 import Button from '@coralui/react-coral/lib/Button';
 import Textfield from '@coralui/redux-form-react-coral/lib/Textfield';
